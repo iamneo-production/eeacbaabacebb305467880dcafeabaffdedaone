fe4e274df912348e0fb04742fa488daa9918ec62
Iamneo <techadmin@iamneo.ai>
2023-09-27 10:08:06 +0000

    Backup Commit

---
+++ Project/src/test/java/pages/SignInPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import utils.WebDriverHelper;
+
+public class SignInPage {
+    
+    public SignInPage(WebDriver driver){
+        this.driver = driver;
+    }
+    public void verifySignInPageHeader(){
+
+    }
+    public void sendUserName(String userName) {
+        WebDriverHelper.
+    }
+}
--- Project/src/test/java/runner/dbankApplication.java
+++ Project/src/test/java/runner/dbankApplication.java
@@ -1,5 +1,24 @@
 package runner;
 
-public class dbankApplication {
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import utils.Base;
+
+public class dbankApplication extends Base {
+    @BeforeTest
+    public void startUp() {
+
+    }
+
+    @Test(priority = 1)
+    public void verifySignIn(){
         
+    }
+
+    @AfterTest
+    public void end() {
+
+    }
 }
