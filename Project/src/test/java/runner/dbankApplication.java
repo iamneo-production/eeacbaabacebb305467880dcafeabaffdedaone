4f22f50cd1f7705b73b589a91a22020f1b4354c9
Iamneo <techadmin@iamneo.ai>
2023-09-27 10:23:07 +0000

    Backup Commit

--- Project/src/test/java/runner/dbankApplication.java
+++ Project/src/test/java/runner/dbankApplication.java
@@ -1,20 +1,25 @@
 package runner;
 
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import utils.Base;
 
 public class dbankApplication extends Base {
-    @BeforeTest
-    public void startUp() {
-
+    @BeforeMethod
+    public void startUp() throws MalformedURLException {
+        driver = openBrowser();
+        driver.manage().timeouts().implicitlyWait(Integer.parseInt(null, IMPLICIT_WAIT_TIME, PAGE_LOAD_TIME, IMPLICIT_WAIT_TIME));
     }
 
     @Test(priority = 1)
-    public void verifySignIn(){
-        
+    public void verifySignIn() {
+
     }
 
     @AfterTest
