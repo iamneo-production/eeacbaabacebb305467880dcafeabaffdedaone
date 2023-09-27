b33b91907ae242789d3ca637479f9e8478deadfb
Iamneo <techadmin@iamneo.ai>
2023-09-27 11:26:43 +0000

    Backup Commit

--- Project/src/test/java/runner/dbankApplication.java
+++ Project/src/test/java/runner/dbankApplication.java
@@ -5,6 +5,7 @@ import java.net.MalformedURLException;
 import java.util.logging.Logger;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -52,9 +53,13 @@ public class dbankApplication extends Base {
         }
 
     }
+    @
 
-    @AfterTest
+    @AfterMethod
     public void end() {
-
+        driver.quit();
+        extentReport.flush();
+        log.info("Browser closed");
+        LoggerHandler.closeHandler();
     }
 }
